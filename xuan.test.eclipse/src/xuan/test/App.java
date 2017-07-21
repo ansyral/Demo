7990feeba62aef7095a738cf805c5c9ba5afb8d6
xuzho <xuzho@microsoft.com>
2017-07-21 17:31:14 +0800

    add sample test for demo

---
+++ xuan.test.eclipse/src/xuan/test/App.java
@@ -0,0 +1,10 @@
+package xuan.test;
+
+public class App {
+	public static void main( String[] args )
+    {
+        String s = "tt";
+        System.out.println( "Hello World!" );
+        int ss = 23;
+    }
+}
---
+++ xuan.test.mavenpluginNoM2esupport/src/main/java/xuan/test/App.java
@@ -0,0 +1,20 @@
+package xuan.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        test1();
+        String st = "sss";
+    }
+
+    private static void test1()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
