4bc53e57812ec5302b61e02d64f91bf358d37da0
xuzho <xuzho@microsoft.com>
2017-07-24 13:45:44 +0800

    add a mven project whose dependency isn't configured correctly

---
+++ xuan.test.mavenpluginmissingdep/src/main/java/xuan/test/App.java
@@ -0,0 +1,15 @@
+package xuan.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String s = "tt";
+        System.out.println( "Hello World!" );
+        int ss = 23;
+    }
+}
