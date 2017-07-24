26ddffb21bce89ff195fb3586bded55b60bc9c52
xuzho <xuzho@microsoft.com>
2017-07-24 17:04:27 +0800

    update launch.json to remove classpath

--- xuan.test.eclipse/src/xuan/test/App.java
+++ xuan.test.eclipse/src/xuan/test/App.java
@@ -3,7 +3,7 @@ package xuan.test;
 public class App {
 	public static void main( String[] args )
     {
-        String s = "tt";
+        String s = "";
         System.out.println( "Hello World!" );
         int ss = 23;
     }
