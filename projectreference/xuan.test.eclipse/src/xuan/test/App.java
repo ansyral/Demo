1a27fe609860a434d47b6994b888dd0164cba10a
xuzho <xuzho@microsoft.com>
2017-07-24 17:42:40 +0800

    add an example for project reference

---
+++ projectreference/xuan.test.eclipse/src/xuan/test/App.java
@@ -0,0 +1,15 @@
+package xuan.test;
+
+public class App {
+	public static void main( String[] args )
+    {
+        String s = "";
+        System.out.println( "Hello World!" );
+        int ss = 23;
+    }
+
+    public void test()
+    {
+        System.out.println("test method");
+    }
+}
---
+++ projectreference/xuan.test.eclipseWithProjectReference/src/xuan/test/eclipseWithProjectReference/App2.java
@@ -0,0 +1,12 @@
+package xuan.test.eclipseWithProjectReference;
+
+import xuan.test.App;
+public class App2 {
+	public static void main( String[] args )
+    {
+        String s = "tt";
+        System.out.println( "Hello World!" );
+        int ss = 23;
+        new App().test();
+    }
+}
